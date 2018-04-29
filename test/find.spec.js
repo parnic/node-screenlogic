26083f75a7bcdc7b3d32cef40b8c3b1f8a2c463e
Parnic <github@parnic.com>
2018-04-28 22:41:38 -0500

    Added very basic tests

---
+++ test/find.spec.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const ScreenLogic = require('../index');
+
+// you'll need a ScreenLogic-enabled device on your network for this to succeed
+describe('Finder', () => {
+  it('finds a server', done => {
+    let finder = new ScreenLogic.FindUnits();
+    finder.on('serverFound', server => {
+      finder.close()
+      done()
+    })
+    finder.search();
+  })
+})
---
+++ test/unit.spec.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const ScreenLogic = require('../index');
+
+// you'll need a ScreenLogic-enabled device on your network for this to succeed
+describe('Unit', () => {
+  let unit
+  before(done => {
+    let finder = new ScreenLogic.FindUnits();
+    finder.on('serverFound', server => {
+      finder.close()
+
+      unit = new ScreenLogic.UnitConnection(server)
+      unit.on('loggedIn', () => {
+        done()
+      })
+
+      unit.connect()
+    })
+
+    finder.search();
+  })
+
+  after(() => {
+    unit.close()
+  })
+
+  let circuit
+  it('gets pool status', done => {
+    unit.on('poolStatus', status => {
+      circuit = status.circuitArray[0]
+      done()
+    })
+
+    unit.getPoolStatus()
+  })
+
+  it('gets controller config', done => {
+    unit.on('controllerConfig', config => { done() })
+    unit.getControllerConfig()
+  })
+
+  it('gets chemical data', done => {
+    unit.on('chemicalData', () => { done() })
+    unit.getChemicalData()
+  })
+
+  it('gets salt cell config', done => {
+    unit.on('saltCellConfig', () => { done() })
+    unit.getSaltCellConfig()
+  })
+
+  it('gets version', done => {
+    unit.on('version', () => { done() })
+    unit.getVersion()
+  })
+
+  it('sets circuit state', done => {
+    unit.on('circuitStateChanged', () => { done() })
+    unit.setCircuitState(0, circuit.id, circuit.state)
+  })
+})
