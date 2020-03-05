43725ae0831aa8621a0611667eea3d9c4b684ecd
Parnic <github@parnic.com>
2020-03-04 21:56:06 -0600

    Fixed remote login
    I forgot that this message sometimes receives a string instead of a buffer.
    
    Fixes #19

--- messages/SLGetGatewayDataMessage.js
+++ messages/SLGetGatewayDataMessage.js
@@ -7,7 +7,7 @@ const MSG_ID = 18003;
 exports.SLGetGatewayDataMessage = class SLGetGatewayDataMessage extends SLMessage {
   constructor(buf) {
     var size;
-    if (buf) {
+    if (buf && typeof buf === 'object') {
       size = buf.readInt32LE(4) + 8;
     }
     super(0, MSG_ID, size);
