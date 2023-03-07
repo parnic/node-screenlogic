902aec7613d983ce6f3729a0d5181532b0b88e53
Parnic <github@parnic.com>
2023-03-06 20:47:00 -0600

    Update example
    This more or less restores to the previous version of the example code under the v2 api.

--- example.ts
+++ example.ts
@@ -1,187 +1,59 @@
-'use strict';
+import { FindUnits, LocalUnit/*, RemoteLogin*/ } from './index';
+import { UnitConnection } from './index';
+// import { SLGateWayData } from "./messages/SLGatewayDataMessage";
 
-import { RemoteLogin, FindUnits, screenlogic, LocalUnit } from './index';
-import { SLGateWayData } from './messages/SLGatewayDataMessage';
-
+// to find units on the local network with the event-based approach:
 const finder = new FindUnits();
-// use this to find and connect to units local to the network this is running on
-// finder.on('serverFound', function(server) {
-//  connect(new ScreenLogic.UnitConnection(server));
-// });
-//
-// finder.search();
-  
-async function app() {
+finder.once('serverFound', async (unit: LocalUnit) => {
   finder.close();
+  console.log(`Found unit: ${JSON.stringify(unit)}`);
+  await connect(unit.gatewayName, unit.address, unit.port);
+});
+finder.search();
 
-  // use this if you want to use a direct connection to a known unit
-  // connect(new ScreenLogic.UnitConnection('10.0.0.85', 80));
+// or to connect directly to an already-known unit using async/await (assuming an appropriate environment for await):
+// await connect('', '10.0.0.85', 80);
 
-  // use this to remote connect to a system by name (going through the Pentair servers)
+// or to gather all the units that can be found in 1 second using Promises:
+// let finder = new FindUnits();
+// finder.searchAsync(1000).then(async (units: LocalUnit[]) => {
+//   if (units.length > 0) {
+//     console.log(`Found unit: ${JSON.stringify(units[0])}`);
+//     await connect(units[0].gatewayName, units[0].address, units[0].port);
+//   }
 
-  let gatewayName = 'Pentair: XX-XX-XX';
-  const password = '1111';
-  let unit: SLGateWayData;
-  try {
-    const finder = new FindUnits();
-    const localUnits: LocalUnit[] = await finder.searchAsync();
-    finder.close();
-    if (localUnits.length) {
-      console.log(`Found units: ${JSON.stringify(localUnits)}`);
-      if (gatewayName.includes('XX-XX-XX')) {
-        gatewayName = localUnits[0].gatewayName;
-        unit.ipAddr = localUnits[0].address;
-        unit.port = localUnits[0].port;
-      }
-    }
-    else console.log('No local units found');
-  }
-  catch (err) {
-    console.error(`Error looking for local units: ${err.message}`);
-  }
+//   process.exit(0);
+// });
 
-  if (!gatewayName.includes('XX-XX-XX')) {
+// or to connect to a system remotely with Promises (set your system name and password):
+// let systemName = 'Pentair: XX-XX-XX';
+// let password = '1234';
+// let gateway = new RemoteLogin(systemName);
+// gateway.connectAsync().then(async (gatewayData: SLGateWayData) => {
+//   if (!gatewayData || !gatewayData.gatewayFound || gatewayData.ipAddr === '') {
+//     console.error(`Screenlogic: No unit found called ${systemName}`);
+//     process.exit(1);
+//   }
 
-    const gateway = new RemoteLogin(gatewayName);
-    unit = await gateway.connectAsync();
+//   await connect(systemName, gatewayData.ipAddr, gatewayData.port, password);
 
-    if (!unit || !unit.gatewayFound || unit.ipAddr === '') {
-      console.log('no unit found by that name');
-      return;
-    }
-    console.log('unit ' + gateway.systemName + ' found at ' + unit.ipAddr + ':' + unit.port);
-  }
-  const client = screenlogic;
-  let delayCount = 0;
-  // events still work, too!
-  // Uncomment to log to the console.
-  /*
-    client.on('loggedIn', function () {
-      console.log(`logged in event`);
-    }).on('version', function (version) {
-      console.log(` version (event)=' + version.version);
-    }).on('equipmentState', function (data) {
-      delayCount = 0; // reset intellibrite delay
-      console.log(`equipmentState (event) update!`)
-      console.log(`Equipment State:`);
-      console.log(JSON.stringify(data));
-    }).on('chemicalData', function (data) {
-      console.log(`chemical data (event):`)
-      console.log(JSON.stringify(data));
-    }).on('intellichlorConfig', function (data) {
-      console.log(`intellchlor (event):`)
-      console.log(JSON.stringify(data));
-    }).on('controllerConfig', function (data) {
-      console.log(`controllerConfig (event):`)
-      console.log(JSON.stringify(data));
-    })
-  */
-  client.on('intellibriteDelay', function (sec: number) {
-    delayCount = delayCount + sec;
-    console.log(`Intellibrite light sequence delay ${delayCount}s`);
-  })
-    .on('loginFailed', function () {
-      console.log(' unable to login (wrong password?)');
-      client.closeAsync();
-    });
+//   process.exit(0);
+// });
 
-  try {
-    client.init(gatewayName, unit.ipAddr, unit.port, password);
-    await client.connectAsync();
-    const addClient = await client.addClientAsync();
-    console.log(`Add client result: ${addClient}`);
+async function connect(gatewayName: string, ipAddr: string, port: number, password?: string) {
+  const client = new UnitConnection();
+  client.init(gatewayName, ipAddr, port, password);
+  await client.connectAsync();
 
-    // EQUIPMENT
-    const equipmentState = await client.equipment.getEquipmentStateAsync();
-    console.log(`Equipment State: ${JSON.stringify(equipmentState)}`);
-    const result = await client.getVersionAsync();
-    console.log(`Pool Version: ${result.version}`);
-    const customNames = await client.equipment.getCustomNamesAsync();
-    console.log(`customNames: ${customNames}`);
-    const controller = await client.equipment.getEquipmentConfigurationAsync();
-    console.log(`Controller: ${JSON.stringify(controller)}`);
-    const equipConfig = await client.equipment.getEquipmentConfigurationAsync();
-    console.log(`Equipment config: ${JSON.stringify(equipConfig)}`);
-    const cancelDelay = await client.equipment.cancelDelayAsync();
-    console.log(`Cancel delay: ${cancelDelay}`);
-    const weatherForecast = await client.equipment.getWeatherForecastAsync();
-    console.log(`Weather: ${JSON.stringify(weatherForecast)}`);
-    const systemTime = await client.equipment.getSystemTimeAsync();
-    console.log(`System Time: ${JSON.stringify(systemTime)}`);
-    const dt = systemTime.date;
-    dt.setHours(12);
-    const sysTime = await screenlogic.equipment.setSystemTimeAsync(dt, true);
-    console.log(`set time result: ${sysTime}`);
-    const hist = await screenlogic.equipment.getHistoryDataAsync();
-    console.log(`history data: ${JSON.stringify(hist)}`);
+  const result = await client.getVersionAsync();
+  console.log(` version=${result.version}`);
 
-    // CHLOR
-    const intellichlor = await client.chlor.getIntellichlorConfigAsync();
-    console.log(`Intellichlor: ${JSON.stringify(intellichlor)}`);
-    const chlorOutput = await client.chlor.setIntellichlorOutputAsync(12, 0);
-    console.log(`Chlor output: ${JSON.stringify(chlorOutput)}`);
+  const state = await client.equipment.getEquipmentStateAsync(42);
+  console.log(` pool temp=${state.bodies[0].currentTemp}`);
+  console.log(` air temp=${state.airTemp}`);
+  console.log(` salt ppm=${state.saltPPM}`);
+  console.log(` pH=${state.pH}`);
+  console.log(` saturation=${state.saturation}`);
 
-    // CHEM
-    const chem = await client.chem.getChemicalDataAsync();
-    console.log(`Chem data: ${JSON.stringify(chem)}`);
-    const chemHist = await screenlogic.chem.getChemHistoryDataAsync();
-    console.log(`history data: ${JSON.stringify(chemHist)}`);
-
-    // SCHEDULES
-    const recurringSched = await client.schedule.getScheduleDataAsync(0);
-    console.log(`reccuring schedules: ${JSON.stringify(recurringSched)}`);
-    const runOnceSched = await client.schedule.getScheduleDataAsync(1);
-    console.log(`Run once schedules: ${JSON.stringify(runOnceSched)}`);
-    // The following will change your settings.  Proceed with Caution
-    /*
-    let addSched = await client.schedule.addNewScheduleEventAsync(SchedTypes.RECURRING);
-    console.log(`Add sched response: ${addSched}`);
-     let setSched = await client.schedule.setScheduleEventByIdAsync(10, 2, 500, 1200, 127, 0, 1, 99);
-    console.log(`Set sched result: ${setSched}`);
-    let delSched = await client.schedule.deleteScheduleEventByIdAsync(10);
-    console.log(`Deleted sched result: ${delSched}`); 
-    */
-
-    // PUMPS
-    const pumpStatus = await client.pump.getPumpStatusAsync(1);
-    console.log(`Pump 1: ${JSON.stringify(pumpStatus)}`);
-    // The following will change your settings.  Proceed with Caution
-
-    const pumpRes = await client.pump.setPumpSpeedAsync(1, 6, 2000, true);
-    console.log(`Pump speed response: ${pumpRes}`);
-
-    // BODIES
-    // The following will change your settings.  Proceed with Caution
-    /*
-    let setPointChanged = await client.bodies.setSetPointAsync(1, 101)
-    console.log(`set point changed: ${setPointChanged}`);
-    let heatModeRes = await client.bodies.setHeatModeAsync(1, HeatModes.HEAT_MODE_HEATPUMP);
-    console.log(`heat mode result: ${heatModeRes}`);
-    */
-
-    // CIRCUITS
-    // The following will change your settings.  Proceed with Caution
-    /*
-    let lightRes = await client.circuits.sendLightCommandAsync(LightCommands.LIGHT_CMD_COLOR_MODE_PARTY);
-    console.log(`Light result: ${lightRes}`);   
-    let cstate = await client.circuits.setCircuitStateAsync(3, true);
-    console.log(`Circuit state: ${JSON.stringify(cstate)}`);
-    let circRun = await client.circuits.setCircuitRuntimebyIdAsync(4, 5);
-    console.log(`circ run res: ${circRun}`);
-*/
-    setTimeout(async () => {
-      console.log('closing connection after 60s');
-      await client.closeAsync();
-    }, 60 * 1000);
-    console.log('Waiting 60s.');
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    client.closeAsync();
-  }
+  await client.closeAsync();
 }
-
-app()
-  .then(() => { null; })
-  .catch(err => {
-    console.log(`App error: ${err.message}`);
-  });
